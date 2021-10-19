1f0b69cafa11af281aa2bb4b664493e66e5ab0cc
jmg <45287417+jeonmingyun@users.noreply.github.com>
2021-10-19 17:59:51 +0900

    Update MainActivity.java
    token 호출 예제 코드 추가

--- app/src/main/java/com/mx/exfcm/MainActivity.java
+++ app/src/main/java/com/mx/exfcm/MainActivity.java
@@ -10,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // token 검색
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        String token = task.getResult();
+                        Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
